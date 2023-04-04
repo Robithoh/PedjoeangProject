1a26dd74b26642494533de118cd6d41a4395a885
Robithoh Akmal <ahmadrobithohakmal@gmail.com>
2023-04-04 14:04:17 +0700

    Pre-Map

---
+++ Assets/Scripts/EnemyCollision.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyCollision : MonoBehaviour
+{
+    private Animator animenemy;
+    public GameObject myTarget;
+    public NavMeshAgent myAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animenemy = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(myTarget != null)
+        {
+            animenemy.SetBool("isMoving", true);
+            myAgent.destination = myTarget.transform.position;
+        }
+        else
+        {
+            animenemy.SetBool("isMoving", false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            myTarget = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            myTarget = null;
+        }
+    }
+}
