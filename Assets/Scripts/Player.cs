82e5da62bfd8fa82cc7e9b69fffbc6fde6cc5672
Robithoh Akmal <ahmadrobithohakmal@gmail.com>
2023-04-04 11:51:45 +0700

    Project Files

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Animator anime;
+    public float speed;
+    public float rotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anime = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput);
+        movement.Normalize();
+
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+
+        if (movement != Vector3.zero)
+        {
+            anime.SetBool("isMoving", true);
+
+            Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            anime.SetBool("isMoving", false);
+        }
+    }
+}
