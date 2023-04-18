7eb0af5c264b41e27f23bd290319b4ee0f7b94f2
Robithoh Akmal <ahmadrobithohakmal@gmail.com>
2023-04-18 11:40:28 +0700

    Coba_AI

---
+++ Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    private Animator animepatrol;
+    public GameObject myTarget;
+    public int range;
+    NavMeshAgent myAgent;
+    public Transform[] waypoints;
+    int waypointIndex;
+    Vector3 target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animepatrol = GetComponent<Animator>();
+        myAgent = GetComponent<NavMeshAgent>();
+        UpdateDestination();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(this.transform.position, myTarget.transform.position);
+
+        if (Vector3.Distance(transform.position, target) < 1)
+        {
+            animepatrol.SetBool("isMoving", true);
+            IterateWaypointIndex();
+            UpdateDestination();
+        }
+        else if (dist < range)
+        {
+            animepatrol.SetBool("isMoving", true);
+            myAgent.destination = myTarget.transform.position;
+        }
+        else
+        {
+            animepatrol.SetBool("isMoving", true);
+            UpdateDestination();
+        }
+    }
+
+    void UpdateDestination()
+    {
+        target = waypoints[waypointIndex].position;
+        myAgent.SetDestination(target);
+    }
+    void IterateWaypointIndex()
+    {
+        waypointIndex++;
+        if (waypointIndex == waypoints.Length)
+        {
+            waypointIndex = 0;
+        }
+    }
+}
