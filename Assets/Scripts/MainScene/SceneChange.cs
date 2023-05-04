97956213c6cf7271fd48c492277383be65ddb622
Robithoh Akmal <ahmadrobithohakmal@gmail.com>
2023-05-04 17:43:34 +0700

    P

--- Assets/Scripts/MainScene/SceneChange.cs
+++ Assets/Scripts/MainScene/SceneChange.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
+    private int currentSceneIndex;
+
     // on trigger enter to change scene
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
+            // saved Scene
+            currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            PlayerPrefs.SetInt("SavedScene", currentSceneIndex);
+
             // change scene
             UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene_TurnBattleSystem");
 
