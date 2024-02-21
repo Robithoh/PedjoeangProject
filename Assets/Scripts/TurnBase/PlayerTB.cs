c38fd7fe43d25e34a0002e12231eb7cd3659418c
Muhammad Zidan <79797943+Funikz@users.noreply.github.com>
2024-02-22 06:08:14 +0700

    scene_OWA

--- Assets/Scripts/TurnBase/PlayerTB.cs
+++ Assets/Scripts/TurnBase/PlayerTB.cs
@@ -247,6 +247,239 @@ public class PlayerTB : MonoBehaviour
         }
     }
 
+    // =============== Skill Cast =============== //
+
+    public GameObject Skill1;
+    public GameObject Skill2;
+
+    public void Cast1OnClick()
+    {
+        foreach (Button button in attackButtons)
+        {
+            button.interactable = false;
+        }
+        StartCoroutine(CastSkill1());
+        StartCoroutine(DelayButton());
+
+    }
+
+    IEnumerator CastSkill1()
+    {
+        ATK = 15;
+        MATK = 12;
+        CRIT = 15;
+        // transform.position = new Vector3(17.139f, 0f, 7.741f);
+        anim.SetBool("isCast", true);
+        Skill1.SetActive(true);
+        yield return new WaitForSeconds(1.5f);
+
+        // Implementasi serangan player ke musuh
+        // Mengurangi HP musuh sejumlah ATK player
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        foreach (GameObject enemy in enemies)
+        {
+            EnemyTB enemyScript = enemy.GetComponent<EnemyTB>();
+            if (enemyScript != null)
+            {
+                Debug.Log("Player Menyerang");
+                float damageDealt = ATK + MATK + CRIT - enemyScript.DEF;
+                damageDealt = Mathf.Max(0, damageDealt); // Pastikan damage tidak negatif
+                enemyScript.TakeDamage(damageDealt);
+                transform.position = new Vector3(14.969f, 0f, 6.585823f);
+                Skill1.SetActive(false);
+                anim.SetBool("isCast", false);
+            }
+        }
+    }
+    public void Cast2OnClick()
+    {
+        foreach (Button button in attackButtons)
+        {
+            button.interactable = false;
+        }
+        StartCoroutine(CastSkill2());
+        StartCoroutine(DelayButton());
+    }
+
+    IEnumerator CastSkill2()
+    {
+        ATK = 15;
+        MATK = 12;
+        CRIT = 15;
+        // transform.position = new Vector3(17.139f, 0f, 7.741f);
+        anim.SetBool("isCast", true);
+        Skill2.SetActive(true);
+        yield return new WaitForSeconds(1.5f);
+
+        // Implementasi serangan player ke musuh
+        // Mengurangi HP musuh sejumlah ATK player
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        foreach (GameObject enemy in enemies)
+        {
+            EnemyTB enemyScript = enemy.GetComponent<EnemyTB>();
+            if (enemyScript != null)
+            {
+                Debug.Log("Player Menyerang");
+                float damageDealt = ATK + MATK + CRIT - enemyScript.DEF;
+                damageDealt = Mathf.Max(0, damageDealt); // Pastikan damage tidak negatif
+                enemyScript.TakeDamage(damageDealt);
+                Skill2.SetActive(false);
+                transform.position = new Vector3(14.969f, 0f, 6.585823f);
+                anim.SetBool("isCast", false);
+            }
+        }
+    }
+    public void Cast3OnClick()
+    {
+        foreach (Button button in attackButtons)
+        {
+            button.interactable = false;
+        }
+        StartCoroutine(CastSkill3());
+        StartCoroutine(DelayButton());
+    }
+
+    IEnumerator CastSkill3()
+    {
+        ATK = 15;
+        MATK = 12;
+        CRIT = 15;
+        // transform.position = new Vector3(17.139f, 0f, 7.741f);
+        anim.SetBool("isCast", true);
+        yield return new WaitForSeconds(1.5f);
+
+        // Implementasi serangan player ke musuh
+        // Mengurangi HP musuh sejumlah ATK player
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        foreach (GameObject enemy in enemies)
+        {
+            EnemyTB enemyScript = enemy.GetComponent<EnemyTB>();
+            if (enemyScript != null)
+            {
+                Debug.Log("Player Menyerang");
+                float damageDealt = ATK + MATK + CRIT - enemyScript.DEF;
+                damageDealt = Mathf.Max(0, damageDealt); // Pastikan damage tidak negatif
+                enemyScript.TakeDamage(damageDealt);
+                transform.position = new Vector3(14.969f, 0f, 6.585823f);
+                anim.SetBool("isCast", false);
+            }
+        }
+    }
+    public void Cast4OnClick()
+    {
+        foreach (Button button in attackButtons)
+        {
+            button.interactable = false;
+        }
+        StartCoroutine(CastSkill4());
+        StartCoroutine(DelayButton());
+    }
+
+    IEnumerator CastSkill4()
+    {
+        ATK = 15;
+        MATK = 12;
+        CRIT = 15;
+        // transform.position = new Vector3(17.139f, 0f, 7.741f);
+        anim.SetBool("isCast", true);
+        yield return new WaitForSeconds(1.5f);
+
+        // Implementasi serangan player ke musuh
+        // Mengurangi HP musuh sejumlah ATK player
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        foreach (GameObject enemy in enemies)
+        {
+            EnemyTB enemyScript = enemy.GetComponent<EnemyTB>();
+            if (enemyScript != null)
+            {
+                Debug.Log("Player Menyerang");
+                float damageDealt = ATK + MATK + CRIT - enemyScript.DEF;
+                damageDealt = Mathf.Max(0, damageDealt); // Pastikan damage tidak negatif
+                enemyScript.TakeDamage(damageDealt);
+                transform.position = new Vector3(14.969f, 0f, 6.585823f);
+                anim.SetBool("isCast", false);
+            }
+        }
+    }
+    public void Cast5OnClick()
+    {
+        foreach (Button button in attackButtons)
+        {
+            button.interactable = false;
+        }
+        StartCoroutine(CastSkill5());
+        StartCoroutine(DelayButton());
+    }
+
+    IEnumerator CastSkill5()
+    {
+        ATK = 15;
+        MATK = 12;
+        CRIT = 15;
+        // transform.position = new Vector3(17.139f, 0f, 7.741f);
+        anim.SetBool("isCast", true);
+        yield return new WaitForSeconds(1.5f);
+
+        // Implementasi serangan player ke musuh
+        // Mengurangi HP musuh sejumlah ATK player
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        foreach (GameObject enemy in enemies)
+        {
+            EnemyTB enemyScript = enemy.GetComponent<EnemyTB>();
+            if (enemyScript != null)
+            {
+                Debug.Log("Player Menyerang");
+                float damageDealt = ATK + MATK + CRIT - enemyScript.DEF;
+                damageDealt = Mathf.Max(0, damageDealt); // Pastikan damage tidak negatif
+                enemyScript.TakeDamage(damageDealt);
+                transform.position = new Vector3(14.969f, 0f, 6.585823f);
+                anim.SetBool("isCast", false);
+            }
+        }
+    }
+    public void Cast6OnClick()
+    {
+        foreach (Button button in attackButtons)
+        {
+            button.interactable = false;
+        }
+        StartCoroutine(CastSkill6());
+        StartCoroutine(DelayButton());
+    }
+
+    IEnumerator CastSkill6()
+    {
+        ATK = 15;
+        MATK = 12;
+        CRIT = 15;
+        // transform.position = new Vector3(17.139f, 0f, 7.741f);
+        anim.SetBool("isCast", true);
+        yield return new WaitForSeconds(1.5f);
+
+        // Implementasi serangan player ke musuh
+        // Mengurangi HP musuh sejumlah ATK player
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        foreach (GameObject enemy in enemies)
+        {
+            EnemyTB enemyScript = enemy.GetComponent<EnemyTB>();
+            if (enemyScript != null)
+            {
+                Debug.Log("Player Menyerang");
+                float damageDealt = ATK + MATK + CRIT - enemyScript.DEF;
+                damageDealt = Mathf.Max(0, damageDealt); // Pastikan damage tidak negatif
+                enemyScript.TakeDamage(damageDealt);
+                transform.position = new Vector3(14.969f, 0f, 6.585823f);
+                anim.SetBool("isCast", false);
+            }
+        }
+    }
+
     IEnumerator DelayButton()
     {
         yield return new WaitForSeconds(7f);
